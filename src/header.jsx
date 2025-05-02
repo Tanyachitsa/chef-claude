49e1e475c8fbb2aee0bba776bd5d87fa59c6b617
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-05-02 14:02:45 +0100

    Added icon to header

--- src/header.jsx
+++ src/header.jsx
@@ -1,6 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKitchenSet } from "@fortawesome/free-solid-svg-icons";
+
 export default function Header() {
   return (
     <header className="header-section">
+      <FontAwesomeIcon icon={faKitchenSet} className="header-icon" />
       <span className="title">Chef Claude</span>
     </header>
   );
